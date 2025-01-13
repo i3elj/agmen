146afbc02f5303dbf068689d300fd2aeec9534d7
r-odriguez <guisande.bernardo@gmail.com>
2025-01-13 07:56:43 -0300

    no more api specific function + fixing config

--- src/Route.php
+++ src/Route.php
@@ -6,162 +6,143 @@ use function tusk\http\header\redirect;
 
 class Route
 {
-    public readonly string $path;
-    private array $params = [];
-    private string $route;
+	public readonly string $path;
+	private array $params = [];
+	private string $route;
 
-    public function __construct()
-    {
-        $this->path = URL['path'];
-    }
+	public function __construct()
+	{
+		$this->path = URL['path'];
+	}
 
-    /**
-     * Redirects the request to the specified path
-     *
-     * @param string $from
-     * @param string $to
-     *
-     * @return Route
-     */
-    public function redirect($from, $to)
-    {
-        if ($this->path === $from) redirect($to);
-        return $this;
-    }
+	/**
+	 * Redirects the request to the specified path
+	 *
+	 * @param string $from
+	 * @param string $to
+	 *
+	 * @return Route
+	 */
+	public function redirect($from, $to)
+	{
+		if ($this->path === $from) redirect($to);
+		return $this;
+	}
 
-    /**
-     * Forwards the request to the specified controller
-     *
-     * @param string $route The expected requested path.
-     * @param string $controller_route The route to the controller.
-     *
-     * @return Route
-     */
-    public function path($route, $controller_route)
-    {
-        if ($this->parse_url_params($route) || $this->path === $route) {
-            $this->route = $this->remove_params($route);
-            controller($controller_route, prefix: \WEB_DIR);
-            exit(0);
-        }
+	/**
+	 * Forwards the request to the specified controller
+	 *
+	 * @param string $route The expected requested path.
+	 * @param string $controller_route The route to the controller.
+	 *
+	 * @return Route
+	 */
+	public function path($route, $controller_route)
+	{
+		if ($this->parse_url_params($route) || $this->path === $route) {
+			$this->route = $this->remove_params($route);
+			controller($controller_route, prefix: \WEB_DIR);
+			exit(0);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Forwards the request to the specified controller
-     *
-     * @param string $route The expected requested path.
-     * @param string $controller_route The route to the controller.
-     * @param array $middlewares Every middleware that will be applied to the route.
-     *
-     * @return Route
-     */
-    public function pathM($route, $controller_route, ...$middlewares)
-    {
-        if ($this->parse_url_params($route) || $this->path === $route) {
-            $this->route = $this->remove_params($route);
-            middleware($controller_route, \WEB_DIR, $middlewares);
-            exit(0);
-        }
+	/**
+	 * Forwards the request to the specified controller
+	 *
+	 * @param string $route The expected requested path.
+	 * @param string $controller_route The route to the controller.
+	 * @param array $middlewares Every middleware that will be applied to the route.
+	 *
+	 * @return Route
+	 */
+	public function pathM($route, $controller_route, ...$middlewares)
+	{
+		if ($this->parse_url_params($route) || $this->path === $route) {
+			$this->route = $this->remove_params($route);
+			middleware($controller_route, \WEB_DIR, $middlewares);
+			exit(0);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Forwards the request to the specified controller
-     *
-     * @param string $route The expected requested path.
-     * @param string $controller_route The route to the controller.
-     *
-     * @return Route
-     */
-    public function api($route, $controller_route)
-    {
-        if ($this->parse_url_params($route) || $this->path === $route) {
-            $this->route = $this->remove_params($route);
-            controller($controller_route, prefix: \API_DIR);
-            exit(0);
-        }
+	/**
+	 * Gets a specific url parameter by name.
+	 *
+	 * @param string $name The name of the url parameter.
+	 *
+	 * @return string|number|null
+	 */
+	public function param($name)
+	{
+		if (array_key_exists($name, $this->params)) {
+			return $this->params[$name];
+		}
 
-        return $this;
-    }
+		return null;
+	}
 
-    /**
-     * Gets a specific url parameter by name.
-     *
-     * @param string $name The name of the url parameter.
-     *
-     * @return string|number|null
-     */
-    public function param($name)
-    {
-        if (array_key_exists($name, $this->params)) {
-            return $this->params[$name];
-        }
+	/**
+	 * Resolves a view using the route passed to the `Route::path` method.
+	 *
+	 * @param array $ctx Any variable that should be available in the path.
+	 *
+	 * @return void
+	 */
+	public function view($ctx = [])
+	{
+		extract($ctx, EXTR_SKIP);
+		require_once base_path(\WEB_DIR . $this->route. "view.php");
+	}
 
-        return null;
-    }
+	/**
+	 * @return boolean
+	 */
+	private function parse_url_params($route)
+	{
+		$var_regex = ':([A-Za-z]+)';
+		$type_regex = '\((word|number)\)';
+		$var_type_regex = '/' . $var_regex . $type_regex . '/';
 
-    /**
-     * Resolves a view using the route passed to the `Route::path` method.
-     *
-     * @param array $ctx Any variable that should be available in the path.
-     *
-     * @return void
-     */
-    public function view($ctx = [])
-    {
-        extract($ctx, EXTR_SKIP);
-        require_once base_path(\WEB_DIR . $this->route. "view.php");
-    }
+		if (preg_match($var_type_regex, $route, $output)) {
+			$param = ['name' => $output[1], 'type'=> $output[2]];
+			$replace_type_regex = match ($param['type']) {
+				'word' => '[a-z]+(_[a-z]+)*',
+				'number' => '\d+',
+			};
 
-    /**
-     * @return boolean
-     */
-    private function parse_url_params($route)
-    {
-        $var_regex = ':([A-Za-z]+)';
-        $type_regex = '\((word|number)\)';
-        $var_type_regex = '/' . $var_regex . $type_regex . '/';
+			$type_replace_regex = "/$var_regex($type_regex)?/";
+			$parsed_route = preg_replace($type_replace_regex, "($replace_type_regex)", $route);
+			$parsed_route = preg_replace('/\//', '\/', $parsed_route);
 
-        if (preg_match($var_type_regex, $route, $output)) {
-            $param = ['name' => $output[1], 'type'=> $output[2]];
-            $replace_type_regex = match ($param['type']) {
-                'word' => '[a-z]+(_[a-z]+)*',
-                'number' => '\d+',
-            };
+			// check if the route ends with a route param or not
+			$res = preg_grep("/^.*:[a-z]+\((word|number)\)$/", [$route]);
+			$parsed_route_regex = count($res) > 0 ?
+								  "/$parsed_route$/" :
+								  "/$parsed_route/";
 
-            $type_replace_regex = "/$var_regex($type_regex)?/";
-            $parsed_route = preg_replace($type_replace_regex, "($replace_type_regex)", $route);
-            $parsed_route = preg_replace('/\//', '\/', $parsed_route);
+			preg_match($parsed_route_regex, $this->path, $output);
 
-            // check if the route ends with a route param or not
-            $res = preg_grep("/^.*:[a-z]+\((word|number)\)$/", [$route]);
-            $parsed_route_regex = count($res) > 0 ?
-                                  "/$parsed_route$/" :
-                                  "/$parsed_route/";
+			if (count($output) > 0) {
+				$this->params[$param['name']] = $output[1];
+				return true;
+			}
+		}
 
-            preg_match($parsed_route_regex, $this->path, $output);
+		return false;
+	}
 
-            if (count($output) > 0) {
-                $this->params[$param['name']] = $output[1];
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $route
-     */
-    private function remove_params($route)
-    {
-        return preg_replace(
-            "/:[a-z]+\((word|number)\)(\/)?/",
-            "",
-            $route
-        );
-    }
+	/**
+	 * @param string $route
+	 */
+	private function remove_params($route)
+	{
+		return preg_replace(
+			"/:[a-z]+\((word|number)\)(\/)?/",
+			"",
+			$route
+		);
+	}
 }
