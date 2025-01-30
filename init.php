f522f8c515bc9ac8367caad94c553c8f7cbe4032
r-odriguez <guisande.bernardo@gmail.com>
2025-01-30 13:44:13 -0300

    REFACTOR:why not just use define anyway

--- init.php
+++ init.php
@@ -4,11 +4,7 @@ if (array_key_exists('REQUEST_URI', $_SERVER)) {
     define("URL", parse_url($_SERVER['REQUEST_URI']));
 }
 
-$vars = require __DIR__ . '/../../config.php';
-
-foreach ($vars as $key => $value)
-    define("$key", $value);
-
+require __DIR__ . '/../../config.php';
 require_once __DIR__ . "/src/global_functions.php";
 
 import(__DIR__ . '/src', index: 1);
