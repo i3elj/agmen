fa143b830ca48f797af6ea8f4a94b1f41649c76e
r-odriguez <guisande.bernardo@gmail.com>
2025-01-31 20:40:57 -0300

    FIX:fixing path

--- init.php
+++ init.php
@@ -4,7 +4,7 @@ if (array_key_exists('REQUEST_URI', $_SERVER)) {
     define("URL", parse_url($_SERVER['REQUEST_URI']));
 }
 
-require __DIR__ . '/../../config.php';
+require __DIR__ . '/../../../config.php';
 require_once __DIR__ . "/src/global_functions.php";
 
 import(__DIR__ . '/src', index: 1);
