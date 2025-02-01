393404f71d7be149c6c3651f2fb60f59aa1f8977
r-odriguez <guisande.bernardo@gmail.com>
2025-01-31 23:13:19 -0300

    REFACTOR:now the definition of global variables aren't required anymore

--- init.php
+++ init.php
@@ -4,7 +4,14 @@ if (array_key_exists('REQUEST_URI', $_SERVER)) {
     define("URL", parse_url($_SERVER['REQUEST_URI']));
 }
 
-require __DIR__ . '/../../../config.php';
+include __DIR__ . '/../../../config.php';
 require_once __DIR__ . "/src/global_functions.php";
+
+if (!isset(WEB_DIR)) define("WEB_DIR", "src/www");
+if (!isset(COMPONENTS_DIR_NAME)) define("COMPONENTS_DIR_NAME", "partials");
+if (!isset(GLOBALS_DIR)) define("GLOBALS_DIR", "src/globals");
+if (!isset(ICONS_DIR)) define("ICONS_DIR", "public/svg/icons");
+if (!isset(SVG_DIR)) define("SVG_DIR", "public/svg");
+if (!isset(ERROR_PAGES_DIR)) define("ERROR_PAGES_DIR", "src/www/errors");
 
 import(__DIR__ . '/src', index: 1);
