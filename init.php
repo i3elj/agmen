71f9991e6dc854b675acb0114905c5fb5b279528
r-odriguez <guisande.bernardo@gmail.com>
2025-02-01 00:11:43 -0300

    FIX:fixed stupid error with isset vs defined

--- init.php
+++ init.php
@@ -7,11 +7,11 @@ if (array_key_exists('REQUEST_URI', $_SERVER)) {
 include __DIR__ . '/../../../config.php';
 require_once __DIR__ . "/src/global_functions.php";
 
-if (!isset(WEB_DIR)) define("WEB_DIR", "src/www");
-if (!isset(COMPONENTS_DIR_NAME)) define("COMPONENTS_DIR_NAME", "partials");
-if (!isset(GLOBALS_DIR)) define("GLOBALS_DIR", "src/globals");
-if (!isset(ICONS_DIR)) define("ICONS_DIR", "public/svg/icons");
-if (!isset(SVG_DIR)) define("SVG_DIR", "public/svg");
-if (!isset(ERROR_PAGES_DIR)) define("ERROR_PAGES_DIR", "src/www/errors");
+if (!defined('WEB_DIR')) define("WEB_DIR", "src/www");
+if (!defined('COMPONENTS_DIR_NAME')) define("COMPONENTS_DIR_NAME", "partials");
+if (!defined('GLOBALS_DIR')) define("GLOBALS_DIR", "src/globals");
+if (!defined('ICONS_DIR')) define("ICONS_DIR", "public/svg/icons");
+if (!defined('SVG_DIR')) define("SVG_DIR", "public/svg");
+if (!defined('ERROR_PAGES_DIR')) define("ERROR_PAGES_DIR", "src/www/errors");
 
 import(__DIR__ . '/src', index: 1);
