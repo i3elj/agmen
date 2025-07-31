2a0b6a3959f0a823d435da8bcbbb07bf2c81de8a
r-odriguez <guisande.bernardo@gmail.com>
2025-07-31 18:08:45 -0300

    refactor: base_path is defined in the init file so libraries that load tusk won't result with errors

--- init.php
+++ init.php
@@ -1,17 +1,19 @@
 <?php declare(strict_types=1);
 
 if (array_key_exists('REQUEST_URI', $_SERVER)) {
-    define("URL", parse_url($_SERVER['REQUEST_URI']));
+    define('URL', parse_url($_SERVER['REQUEST_URI']));
 }
 
-include __DIR__ . '/../../../config.php';
-require_once __DIR__ . "/src/global_functions.php";
+define('BASE_PATH', __DIR__ . '/../../../');
 
-if (!defined('WEB_DIR')) define("WEB_DIR", "src/www");
-if (!defined('COMPONENTS_DIR_NAME')) define("COMPONENTS_DIR_NAME", "partials");
-if (!defined('GLOBALS_DIR')) define("GLOBALS_DIR", "src/globals");
-if (!defined('ICONS_DIR')) define("ICONS_DIR", "public/svg/icons");
-if (!defined('SVG_DIR')) define("SVG_DIR", "public/svg");
-if (!defined('ERROR_PAGES_DIR')) define("ERROR_PAGES_DIR", "src/www/errors");
+include BASE_PATH . 'config.php';
+require_once __DIR__ . '/src/global_functions.php';
+
+if (!defined('WEB_DIR'))             define('WEB_DIR'             , 'src/www');
+if (!defined('COMPONENTS_DIR_NAME')) define('COMPONENTS_DIR_NAME' , 'partials');
+if (!defined('GLOBALS_DIR'))         define('GLOBALS_DIR'         , 'src/globals');
+if (!defined('ICONS_DIR'))           define('ICONS_DIR'           , 'public/svg/icons');
+if (!defined('SVG_DIR'))             define('SVG_DIR'             , 'public/svg');
+if (!defined('ERROR_PAGES_DIR'))     define('ERROR_PAGES_DIR'     , 'src/www/errors');
 
 import(__DIR__ . '/src', index: 1);
