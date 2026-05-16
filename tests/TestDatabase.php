ccce0982facd778bd3bbb21724540a366dd7ccc3
i3elj <guisande.bernardo@gmail.com>
2026-05-16 12:43:37 -0300

    new: tests files

---
+++ tests/TestDatabase.php
@@ -0,0 +1,21 @@
+<?php
+define('DEV_MODE', true);
+define('BASE_PATH', dirname(__DIR__) . '/');
+define('ENV', [
+	'DB'      => 'mysql',
+	'DB_HOST' => 'localhost',
+	'DB_PORT' => '8080',
+	'DB_USER' => 'john.doe',
+	'DB_PWD'  => '123',
+	'DB_NAME' => 'database',
+]);
+
+require 'bootstrap.php';
+require '../init.php';
+require '../src/global_functions.php';
+
+use Agmen\Database;
+
+$db = Database::connect();
+[$rows, $count] = $db->sqlr("SELECT * FROM test");
+Agmen\dd($rows);
---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+spl_autoload_register(function ($class) {
+	$class = str_replace('\\', '/', $class);
+	$class = str_replace('Agmen/', '', $class);
+	$file = __DIR__ . '/../src/' . $class . '.php';
+
+	if (file_exists($file)) {
+		require_once $file;
+	}
+});
